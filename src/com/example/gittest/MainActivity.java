aafc6ad9ea6c33ab695abe81045a40f9efd3755f
splm <splm_lis@163.com>
2015-08-26 14:32:12 +0800

    '获取了id为hello的控件为它重新设置了文本'

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	private TextView hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello=(TextView)findViewById(R.id.hello);
+        hello.setText("这是一个测试标题");
     }
 }
