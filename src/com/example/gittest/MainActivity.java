ba7c3cf9280c8dba2a7e7b498280ea11d0a94d32
splm <splm_lis@163.com>
2015-08-26 15:04:59 +0800

    '为了文本框加入了单击事件'

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gittest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -12,5 +14,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         hello=(TextView)findViewById(R.id.hello);
         hello.setText("这是一个测试标题");
+        hello.setOnClickListener(new CustomOnClickListener());
+    }
+    private class CustomOnClickListener implements OnClickListener{
+
+		@Override
+		public void onClick(View arg0) {
+			
+			System.out.println("单击了这个文本");
+			
+		}
+    	
     }
 }
+
