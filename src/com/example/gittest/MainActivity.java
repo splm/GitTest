a90f4bbbd1619c261d7821d53ddb3a7f3b0bc0fe
splm <splm_lis@163.com>
2015-09-02 17:20:28 +0800

    加入了测试代码

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.gittest;
 
+import com.example.gittest.model.UserModel;
+import com.example.gittest.utils.LogHelper;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -7,6 +10,9 @@ import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
 	private TextView hello;
+	
+	private UserModel userModel;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	
@@ -27,6 +33,14 @@ public class MainActivity extends BaseActivity {
 		
 		hello.setText("这是一个测试标题");
 		
+		userModel=new UserModel();
+		
+		userModel.setUsername("张三");
+		
+		userModel.setUsersex("男");
+		
+		userModel.setUsertype("管理员");
+		
 	}
 	@Override
 	protected void bindEvent() {
@@ -39,6 +53,24 @@ public class MainActivity extends BaseActivity {
 
 		@Override
 		public void onClick(View arg0) {
+			
+			try {
+				
+				UserModel cloneUserModel=(UserModel) userModel.clone();
+				
+				cloneUserModel.setUsername("李四");
+				
+				cloneUserModel.setUsersex("女");
+				
+				LogHelper.traceField("原型：姓名\n:"+userModel.getUsername()+"\t性别：\n"+userModel.getUsersex());
+				
+				LogHelper.traceField("克隆：姓名\n:"+cloneUserModel.getUsername()+"\t性别：\n"+cloneUserModel.getUsersex()+"-----"+cloneUserModel.getUsertype());
+				
+			} catch (CloneNotSupportedException e) {
+				
+				e.printStackTrace();
+			
+			}
 			
 			System.out.println("单击了这个文本");
 			
